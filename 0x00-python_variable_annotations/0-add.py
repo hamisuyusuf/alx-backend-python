92bc408a4c974506b8d3462069958cc0cdac3b2e
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-15 23:19:40 +0000

    Task

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
-    """function that takes a float a and a float b as arguments and returns their sum as a float."""
+    """returns their sum as a float."""
     sum: float = a + b
     return sum
