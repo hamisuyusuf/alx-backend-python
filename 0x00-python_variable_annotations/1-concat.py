6e515dcd693e8a1fb6c0b0b8ebebab076a3735d5
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-15 23:34:44 +0000

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+def concat(str1: str, str2: str) -> str:
+    """
+    function that takes a string str1 and str2
+    as arguments and returns a concatenated string
+    """
+    return str1 + str2
